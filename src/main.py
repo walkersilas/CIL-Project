c6b87a31891fe80fba7a0f6c1a27d0d6858191a9
András Geiszl <geiszla@gmail.com>
2021-04-21 11:25:26 +0100

    Fix pre-commit issues

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-"""Main entry point for this project"""
+"""Main entry point for this project."""
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 
+
 def __main(args: Namespace):
-    print(f'This is the data directory: {args.data_dir}')
+    print(f"This is the data directory: {args.data_dir}")
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(
