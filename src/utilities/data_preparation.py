c40b4979c3d17200d3e5adf4b63f8ec5f0562824
Matteo Russo <matteor@princeton.edu>
2021-05-19 09:18:29 +0200

    GNN+NCF

---
+++ src/utilities/data_preparation.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset
+from sklearn.model_selection import train_test_split
+
+
+def load_data(file_path: str, full_dataset: bool, train_val_split: bool, random_seed: int = 0, train_size: float = 0):
+    data_pd = pd.read_csv(file_path)
+
+    # Reduce Dataset for Testing
+    if not full_dataset:
+        data_pd = data_pd.head(10000)
+
+    if train_val_split:
+        train_pd, val_pd = train_test_split(data_pd, train_size=train_size, random_state=random_seed)
+        return train_pd, val_pd
+    else:
+        return data_pd
+
+
+def __extract_users_items_predictions(data_pd: pd.DataFrame):
+    users, movies = \
+        [np.squeeze(arr) for arr in np.split(data_pd.Id.str.extract('r(\d+)_c(\d+)').values.astype(int) - 1, 2, axis=-1)]
+    predictions = data_pd.Prediction.values
+    return users, movies, predictions
+
+
+def __get_tensors_from_dataframe(data_pd: pd.DataFrame):
+    users, movies, predictions = __extract_users_items_predictions(data_pd)
+    users_torch = torch.tensor(users, dtype=torch.int64)
+    movies_torch = torch.tensor(movies, dtype=torch.int64)
+    predictions_torch = torch.tensor(predictions, dtype=torch.int64)
+
+    return users_torch, movies_torch, predictions_torch
+
+
+def create_dataset(data_pd: pd.DataFrame, test_dataset: bool = False):
+    users_torch, movies_torch, predictions_torch = __get_tensors_from_dataframe(data_pd)
+
+    if not test_dataset:
+        return TensorDataset(users_torch, movies_torch, predictions_torch)
+    else:
+        test_ids = data_pd.Id
+        return test_ids, TensorDataset(users_torch, movies_torch)
+
+
+def create_laplacian_matrix(data_pd: pd.DataFrame, number_of_users: int, number_of_movies: int):
+    users_torch, movies_torch, predictions_torch = __get_tensors_from_dataframe(data_pd)
+
+    user_item_matrix = torch.sparse_coo_tensor(torch.vstack((users_torch, movies_torch)), predictions_torch)
+    top_zero_matrix = torch.zeros((user_item_matrix.shape[0], user_item_matrix.shape[0])).to_sparse()
+    bottom_zero_matrix = torch.zeros((user_item_matrix.shape[1], user_item_matrix.shape[1])).to_sparse()
+
+    top_a = torch.cat((top_zero_matrix, user_item_matrix), dim=1)
+    bottom_a = torch.cat((torch.transpose(user_item_matrix, 0, 1), bottom_zero_matrix), dim=1)
+    matrix_a = torch.vstack((top_a, bottom_a))
+
+    degree = (matrix_a.to_dense() > 0).sum(axis=1)
+    degree_matrix = torch.diag(torch.pow(degree, -0.5))
+
+    laplacian_matrix = torch.sparse.mm(degree_matrix, torch.sparse.mm(matrix_a, degree_matrix)).to_sparse()
+    return laplacian_matrix
---
+++ src/utilities/helper.py
@@ -0,0 +1,88 @@
+from argparse import (
+    ArgumentDefaultsHelpFormatter,
+    ArgumentParser,
+    Namespace
+)
+import json
+from pytorch_lightning.loggers import CometLogger
+
+
+def create_argument_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        description="Main entry point for this project",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        default="../../data/",
+        help="path to the directory containing the unprocessed data"
+    )
+    parser.add_argument(
+        "--train-data",
+        type=str,
+        default="data_train.csv",
+        help="name of the training data file"
+    )
+    parser.add_argument(
+        "--test-data",
+        type=str,
+        default="data_test.csv",
+        help="name of the testing data file"
+    )
+    parser.add_argument(
+        "--leonhard",
+        action="store_true",
+        help="flag indicating whether the model is run in the leonhard cluster"
+    )
+    parser.add_argument(
+        "--random-seed",
+        type=int,
+        default=7,
+        help="random seed used"
+    )
+    parser.add_argument(
+        "--disable-logging",
+        action="store_true",
+        help="flag indicating whether the experiment is logged in comet ml"
+    )
+    parser.add_argument(
+        "--comet-key",
+        type=str,
+        default="../../comet.json",
+        help="path to the comet api key directory"
+    )
+    parser.add_argument(
+        "--comet-directory",
+        type=str,
+        default="./logs",
+        help="path to log directory when comet can not be run online"
+    )
+    parser.add_argument(
+        "--dataloader-workers",
+        type=int,
+        default=8,
+        help="number of dataloader workers used"
+    )
+    return parser
+
+
+def create_comet_logger(args: Namespace) -> CometLogger:
+    comet_api_key = None
+    try:
+        comet_api_key = json.load(open(args.comet_key))
+    except:
+        print("Comet API Key not found ... Continue by logging the experiment offline ...")
+
+    if comet_api_key is None:
+        return CometLogger(
+            save_dir=args.comet_directory
+        )
+    else:
+        return CometLogger(
+            api_key=comet_api_key["api_key"],
+            project_name=comet_api_key["project_name"],
+            workspace=comet_api_key["workspace"],
+            disabled=args.disable_logging
+        )
