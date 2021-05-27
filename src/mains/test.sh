ee4b629f4e70f85004945b184cbe5320f78292c2
walkers <>
2021-05-27 18:28:44 +0200

    Extend shell script to allow specification of configs

--- src/mains/test.sh
+++ src/mains/test.sh
@@ -1,2 +1,16 @@
-python3 test_reliabilities.py --leonhard
-python3 test.py --leonhard
+#!/usr/bin/env bash
+
+CONFIG1=${1:-none}
+CONFIG2=${2:-none}
+
+if [ "$CONFIG1" = "none" ]; then
+  python3 test_reliabilities.py --leonhard
+else
+  python3 test_reliabilities.py --leonhard --config $CONFIG1
+fi
+
+if [ "$CONFIG2" = "none" ]; then
+    python3 test.py --leonhard
+  else
+    python3 test.py --leonhard --config $CONFIG2
+fi
