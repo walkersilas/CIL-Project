9537960d10937737782b2db20bd684589e9ef97a
walkers <>
2021-06-17 14:28:04 +0200

    Move get_mean_predictions

--- src/mains/ensemble_reinforced_gnn_ncf.sh
+++ src/mains/ensemble_reinforced_gnn_ncf.sh
@@ -7,4 +7,4 @@ python3 main_reinforced_gnn_ncf.py --leonhard --ensemble-learning --random-seed 
 python3 main_reinforced_gnn_ncf.py --leonhard --ensemble-learning --random-seed 841
 python3 main_reinforced_gnn_ncf.py --leonhard --ensemble-learning --random-seed 99
 
-python3 get_mean_predictions.py --ensemble-directory ensemble/reinforced_gnn_ncf
+python3 ../utilities/get_mean_predictions.py --ensemble-directory ensemble/reinforced_gnn_ncf
